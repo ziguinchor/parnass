aab7f255cc4aefeb3ce172f6f3084955bf0fc3dd
macbook <macbook@MacBook-Pro-de-macbook-2.local>
2022-04-30 17:31:27 +0000

    error handling

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const winston = require("winston");
 const logger = require("./utils/logger");
 process.on("unhandledRejection", (err) => {
   logger.error(err);
-  // process.exit(1);
+  process.exit(1);
 });
 
 process.on("uncaughtException", (err) => {
   logger.error(err);
-  // process.exit(1);
+  process.exit(1);
 });
 
 const port = process.env.PORT || 3000;
